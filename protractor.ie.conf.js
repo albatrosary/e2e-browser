20e30aba64e94a3f11ed7c0d8dc12782d9732e09
Fumio SAGAWA <fumio.sagawa@gmail.com>
2018-01-23 15:46:39 +0900

    first commit

---
+++ protractor.ie.conf.js
@@ -0,0 +1,47 @@
+// Protractor configuration file, see link for more information
+// https://github.com/angular/protractor/blob/master/lib/config.ts
+
+const { SpecReporter } = require('jasmine-spec-reporter');
+
+exports.config = {
+  allScriptsTimeout: 11000,
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: [
+    './e2e/**/*.e2e-spec.ts'
+  ],
+  onPrepare: function() {
+    // Disable animations so e2e tests run more quickly
+    var disableNgAnimate = function() {
+      angular.module('disableNgAnimate', []).run(function($animate) {
+        $animate.enabled(false);
+      });
+    };
+
+    browser.addMockModule('disableNgAnimate', disableNgAnimate);
+
+    require('jasmine-reporters');
+    jasmine.getEnv().addReporter(new jasmine.JUnitXmlReporter('xmloutput', true, true));
+  },
+  seleniumServerJar:'./node_modules/webdriver-manager/selenium/selenium-server-standalone-3.8.1.jar',
+  capabilities: {
+    browserName: 'internet explorer',
+    maxInstances: 1,
+    seleniumProtocol: "WebDriver",
+    version: "DEFAULT"
+  },
+  directConnect: false,
+  baseUrl: 'http://localhost:4200/',
+  framework: 'jasmine',
+  jasmineNodeOpts: {
+    isVerbose: true,
+    showColors: true,
+    includeStackTrace: true,
+    defaultTimeoutInterval: 30000
+  },
+  onPrepare() {
+    require('ts-node').register({
+      project: 'e2e/tsconfig.e2e.json'
+    });
+    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+  }
+};
